de978e65bc2bd033782e1d8f55f733214ee2eb9d
Rafael Calleja <rafaelcalleja@gmail.com>
2024-01-19 17:55:46 +0100

    using passphrase

--- obfuscator/entrypoint.sh
+++ obfuscator/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+set -a
+
 if [[ ! -z "${REPOSITORY}" ]]; then
   GPG_PASSPHRASE=$(curl -u "$USERNAME:$PASSWORD" -SsL -H "X-Gitea-OTP: $CODE" $REPOSITORY |jq -r .content|base64 -d)
-   if [[ $? -ne 0 ]]; then
-     unset GPG_PASSPHRASE
-   fi
 fi
 
 if [[ ! -z "${GPG_PRIVATE_KEY}" ]] && [[ -z "${GPG_PASSPHRASE}" ]]; then
@@ -14,7 +13,5 @@ fi
 if [[ ! -z "${GPG_PRIVATE_KEY}" ]] && [[ ! -z "${GPG_PASSPHRASE}" ]]; then
   echo ${GPG_PASSPHRASE}| gpg --batch --yes --passphrase-fd 0 --import <(echo ${GPG_PRIVATE_KEY} | base64 --decode) > /dev/null 2>&1
 fi
-
-unset GPG_PASSPHRASE
 
 exec docker-entrypoint.sh "$@"
