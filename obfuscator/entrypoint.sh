b122952c8735dfee070f957d49693d39c777f947
Rafael Calleja <rafaelcalleja@gmail.com>
2024-01-19 13:52:29 +0100

    add passphrase

--- obfuscator/entrypoint.sh
+++ obfuscator/entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [[ ! -z "${GPG_PRIVATE_KEY}" ]]; then
+if [[ ! -z "${REPOSITORY}" ]]; then
+  GPG_PASSPHRASE=$(curl -u "$USERNAME:$PASSWORD" -SsL -H "X-Gitea-OTP: $CODE" $REPOSITORY |jq -r .content|base64 -d)
+   if [[ $? -ne 0 ]]; then
+     unset GPG_PASSPHRASE
+   fi
+fi
+
+if [[ ! -z "${GPG_PRIVATE_KEY}" ]] && [[ -z "${GPG_PASSPHRASE}" ]]; then
   gpg --import <(echo ${GPG_PRIVATE_KEY} | base64 --decode) > /dev/null 2>&1
 fi
+
+if [[ ! -z "${GPG_PRIVATE_KEY}" ]] && [[ ! -z "${GPG_PASSPHRASE}" ]]; then
+  echo ${GPG_PASSPHRASE}| gpg --batch --yes --passphrase-fd 0 --import <(echo ${GPG_PRIVATE_KEY} | base64 --decode) > /dev/null 2>&1
+fi
+
+unset GPG_PASSPHRASE
 
 exec docker-entrypoint.sh "$@"
